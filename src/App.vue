00d05947c19967391aaaf63beca46f0a2cf0396e
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-12-04 16:10:57 +0100

    sv

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,25 @@ export default {
 	},
   methods:{
     findLinks() {
-      
+   
+
+const options = {
+  method: 'GET',
+  url: 'https://api.themoviedb.org/3/authentication',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer 7f49a8a4e093c76db9959c9de6405a5b'
+  }
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
     } 
   }
 }
